bbe382ff6ecaa4570d2446b8d1a17bd886d6d8df
Srushtimalode <srushtimalode98@gmail.com>
2026-07-16 18:39:44 +0530

    Added expresstion_operators.js

---
+++ expresstion_operators.js
@@ -0,0 +1,37 @@
+//Arithmetic Operation 
+let num1 = 10;
+let num2 = 5;
+ console.log("Addition :",num1+num2);
+ console.log("Subtraction:",num1-num2);
+ console.log("multiplication:",num1*num2);
+ console.log("Division:",num1/num2);
+ console.log("modules:",num1%num2);
+
+ //Comparison Operators
+ let a = 10 ,b =20;
+ console.log("gretear than:",a>b);
+ console.log("less than:",a<b);
+ console.log("compare datatype:",a===b);
+ console.log("Not equal to :",a!==b);
+
+//Combine Logical operators
+let isAdult = true;
+let hasLicense = false;
+console.log("AND:",isAdult&&hasLicense);
+console.log("OR",isAdult||hasLicense);
+console.log("NOT",!isAdult);
+
+//Demonstrate Operation 
+let result = 10+2*5-8/4;
+console.log("RESULT:",result);
+
+
+//Assignment Operators
+let x=10;
+x += 5;
+console.log(x);
+x -=3;
+console.log(x);
+x*=2;
+console.log(x);
+
